71ebd2c0f8a93265c5ea1a94a71001c923832eeb
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2013-07-09 05:19:00 +0000

    one more...

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
-import fs
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
   var message = fs.readFileSync('index.html','utf8');
